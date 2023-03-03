96f3dc4e0f91d4c8c608a10ce7120618b517d95f
Emmett Jayhart <23242564+EmmettJayhart@users.noreply.github.com>
2023-03-03 10:23:19 +0000

    More realistic

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,12 @@
 #[cfg(feature = "reflect")]
 use bevy::{ecs::reflect::ReflectResource, reflect::Reflect};
-use bevy::{
-    prelude::{
-        App, Component, Entity, GlobalTransform, Local, Plugin, Quat, Query, Res, Resource, Time,
-        Transform, Vec3, With,
-    },
-    utils::HashMap,
-};
+use bevy::{prelude::*, utils::HashMap};
 
 pub struct BoidsPlugin;
 impl Plugin for BoidsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(move_boid);
+        app.add_system(apply_intent.before(apply_physics))
+            .add_system(apply_physics);
 
         #[cfg(feature = "reflect")]
         app.register_type::<BoidDescriptor>();
@@ -21,23 +16,31 @@ impl Plugin for BoidsPlugin {
 #[derive(Resource)]
 #[cfg_attr(feature = "reflect", derive(Reflect), reflect(Resource))]
 pub struct BoidDescriptor {
-    pub speed: f32,
-    pub minimum_distance: f32,
-    pub maximum_distance: f32,
+    pub thrust: f32,
+    pub lift: f32,
+    pub gravity: f32,
+    pub bank: f32,
+    pub separation: f32,
+    pub alignment: f32,
+    pub cohesion: f32,
+    pub bank_rate: f32,
+    pub rise_rate: f32,
     pub maximum_vision: f32,
-    pub rotational_inertia: f32,
-    pub rotational_energy: f32,
 }
 
 impl Default for BoidDescriptor {
     fn default() -> Self {
         Self {
-            speed: 1.0,
-            minimum_distance: 2.0,
-            maximum_distance: 3.0,
-            maximum_vision: 4.0,
-            rotational_inertia: 1.0,
-            rotational_energy: 1.0,
+            thrust: 2.0,
+            lift: 1.0,
+            gravity: 1.0,
+            bank: 1.0,
+            separation: 70.0,
+            alignment: 1.0,
+            cohesion: 10.0,
+            bank_rate: 0.01,
+            rise_rate: 0.01,
+            maximum_vision: 240.0,
         }
     }
 }
@@ -46,17 +49,14 @@ impl Default for BoidDescriptor {
 #[cfg_attr(feature = "reflect", derive(Reflect))]
 pub struct Boid;
 
-fn move_boid(
+fn apply_intent(
     mut boids_query: Query<(Entity, &mut Transform, &GlobalTransform), With<Boid>>,
-    mut headings: Local<HashMap<Entity, Vec3>>,
+    mut headings: Local<HashMap<Entity, (f32, f32)>>,
     descriptor: Res<BoidDescriptor>,
     time: Res<Time>,
 ) {
-    for (boid, _, global_transform) in boids_query.iter() {
-        let mut separation = Vec3::ZERO;
-        let mut alignment = Vec3::ZERO;
-        let mut cohesion = Vec3::ZERO;
-        let mut minimum_distance = descriptor.maximum_vision;
+    for (boid, transform, global_transform) in boids_query.iter() {
+        let mut heading = Vec3::ZERO;
 
         for (other_boid, _, other_global_transform) in boids_query.iter() {
             if other_boid == boid {
@@ -71,43 +71,42 @@ fn move_boid(
                 continue;
             }
 
-            if distance < minimum_distance {
-                minimum_distance = distance;
-            }
+            let separation =
+                (position - other_position).normalize_or_zero() * descriptor.separation / distance;
+            let alignment = other_global_transform.forward() * descriptor.alignment;
+            let cohesion = (other_position - position).normalize_or_zero() * descriptor.cohesion;
 
-            separation += (position - other_position).normalize_or_zero()
-                * descriptor.minimum_distance
-                / distance;
-
-            alignment += other_global_transform.forward();
-
-            cohesion += (other_position - position).normalize_or_zero() * distance
-                / descriptor.maximum_distance;
+            heading += separation + alignment + cohesion;
         }
 
-        let heading = {
-            if minimum_distance < descriptor.minimum_distance {
-                separation
-            } else if minimum_distance > descriptor.maximum_distance {
-                cohesion
-            } else {
-                alignment
-            }
-        };
+        let lateral =
+            descriptor.bank_rate * (heading.dot(transform.left()) + transform.up().dot(Vec3::Y));
+        let vertical =
+            descriptor.rise_rate * (heading.dot(transform.up()) + transform.up().dot(Vec3::Y));
 
-        headings.insert(boid, heading);
+        headings.insert(boid, (lateral, vertical));
     }
 
     for (boid, mut transform, _) in boids_query.iter_mut() {
-        let heading = (*headings.get(&boid).unwrap_or(&Vec3::ZERO)).normalize_or_zero();
-        let rot =
-            descriptor.rotational_energy * time.delta_seconds() / descriptor.rotational_inertia;
-        let direction = transform
-            .rotation
-            .lerp(Quat::from_rotation_arc(transform.forward(), heading), rot);
-        transform.rotation = direction;
+        let &(lateral, vertical) = headings.get(&boid).unwrap_or(&(0.0, 0.0));
 
-        let displacement = transform.forward() * descriptor.speed * time.delta_seconds();
-        transform.translation += displacement;
+        transform.rotate_local_z(lateral * time.delta_seconds());
+        transform.rotate_local_x(vertical * time.delta_seconds());
+    }
+}
+
+fn apply_physics(
+    mut boids_query: Query<&mut Transform, With<Boid>>,
+    descriptor: Res<BoidDescriptor>,
+    time: Res<Time>,
+) {
+    for mut transform in boids_query.iter_mut() {
+        let thrust = transform.forward() * descriptor.thrust;
+        let lift = transform.up() * descriptor.lift;
+        let gravity = Vec3::NEG_Y * descriptor.gravity;
+        transform.translation += (thrust + lift + gravity) * time.delta_seconds();
+
+        let bank = transform.right().dot(Vec3::Y) * descriptor.bank;
+        transform.rotate_y(bank * time.delta_seconds());
     }
 }
