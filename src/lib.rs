f8e18e489fac9acb2dd66e7f8f3d737c73cb5b16
Emmett Jayhart <23242564+EmmettJayhart@users.noreply.github.com>
2023-04-16 14:14:26 +0100

    Revert to proper end
    This reverts commit f82b3cad96cfd96deaf41d860afb83d0ede35757.
    
    Revert "defaults"
    
    This reverts commit 261baecd36c96103babcd2a10c082147b0ca2ec4.
    
    Revert "Bevy 0.10"
    
    This reverts commit 64e6588741769f2af4f5d5dbe6fa552369070cb7.
    
    Revert "Bump"
    
    This reverts commit cab1e00a6da7b33c8c8d89500ee05164b4a0da9c.
    
    Revert "Spatial RTree optimisation"
    
    This reverts commit 998d59becb394e4b42fd08e8a5581eabab80cd32.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
 #[cfg(feature = "reflect")]
 use bevy::{ecs::reflect::ReflectResource, reflect::Reflect};
 use bevy::{prelude::*, utils::HashMap};
-use bevy_spatial::{RTreeAccess3D, RTreePlugin3D, SpatialAccess};
 
 pub struct BoidsPlugin;
 impl Plugin for BoidsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(RTreePlugin3D::<Boid>::default());
-
         app.add_system(apply_intent.before(apply_physics))
             .add_system(apply_physics);
 
@@ -38,7 +35,7 @@ impl Default for BoidDescriptor {
             lift: 1.0,
             gravity: 1.0,
             bank: 1.0,
-            separation: 100.0,
+            separation: 70.0,
             alignment: 1.0,
             cohesion: 10.0,
             bank_rate: 0.01,
@@ -55,19 +52,25 @@ pub struct Boid;
 fn apply_intent(
     mut boids_query: Query<(Entity, &mut Transform, &GlobalTransform), With<Boid>>,
     mut headings: Local<HashMap<Entity, (f32, f32)>>,
-    tree_access: Res<RTreeAccess3D<Boid>>,
     descriptor: Res<BoidDescriptor>,
     time: Res<Time>,
 ) {
     for (boid, transform, global_transform) in boids_query.iter() {
         let mut heading = Vec3::ZERO;
-        let position = global_transform.translation();
-        let other_boids = tree_access.within_distance(position, descriptor.maximum_vision);
 
-        for &(other_position, other_boid) in other_boids.iter() {
-            let (_, _, other_global_transform) = boids_query.get(other_boid).unwrap();
+        for (other_boid, _, other_global_transform) in boids_query.iter() {
+            if other_boid == boid {
+                continue;
+            }
 
+            let position = global_transform.translation();
+            let other_position = other_global_transform.translation();
             let distance = position.distance(other_position);
+
+            if distance > descriptor.maximum_vision {
+                continue;
+            }
+
             let separation =
                 (position - other_position).normalize_or_zero() * descriptor.separation / distance;
             let alignment = other_global_transform.forward() * descriptor.alignment;
@@ -76,9 +79,8 @@ fn apply_intent(
             heading += separation + alignment + cohesion;
         }
 
-        let lateral = descriptor.bank_rate
-            * ((heading.dot(transform.left()) + transform.up().dot(Vec3::Y))
-                - transform.left().dot(Vec3::Y));
+        let lateral =
+            descriptor.bank_rate * (heading.dot(transform.left()) + transform.up().dot(Vec3::Y));
         let vertical =
             descriptor.rise_rate * (heading.dot(transform.up()) + transform.up().dot(Vec3::Y));
 
